1e9d476ab2a1ddf8e3e090231ed556f0f47c83fd
iigmir <roc120j@gmail.com>
2020-08-20 01:57:36 +0800

    Add btn

--- main.js
+++ main.js
@@ -55,7 +55,8 @@ var app = new Vue({
             selected_wine: [],
             top_picks: [],
             new_arrivals: [],
-            on_top: true
+            on_top: true,
+            mobile_menu_hidden: true,
         }
     },
     computed: {
@@ -70,6 +71,11 @@ var app = new Vue({
         }
     },
     methods: {
+        set_detect_on_top() {
+            if( window.innerWidth >= 900 ) {
+                document.addEventListener( "scroll", this.detect_on_top );
+            }
+        },
         detect_on_top() {
             this.on_top = window.scrollY < 10;
         },
@@ -83,9 +89,24 @@ var app = new Vue({
             this.top_picks = top_ary;
             this.new_arrivals = new_ary;
         },
+        set_mobile_menu_hidden(bool = true) {
+            this.mobile_menu_hidden = bool;
+        },
+        close_menu() {
+            if( !this.mobile_menu_hidden ) {
+                this.set_mobile_menu_hidden(true);
+            }
+        },
+        set_close_menu_by_screen() {
+            if( window.innerWidth < 900 ) {
+                document.addEventListener("click", () => this.close_menu());
+                document.querySelector(".mobile-screen.navigator .hamburger").addEventListener("click", e => e.stopPropagation() );
+            }
+        },
     },
     created() {
-        document.addEventListener( "scroll", this.detect_on_top );
         this.api_request();
+        this.set_detect_on_top();
+        this.set_close_menu_by_screen();
     }
 });
